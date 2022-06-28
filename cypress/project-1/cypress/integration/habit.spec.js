a7bfa45044fa906fadf8ee5b8d36060dd1e553e5
Muhiddin <muhiddinjv@gmail.com>
2022-06-28 13:22:57 +0500

    test: show modal on Add click & show card on Save Changes

--- cypress/project-1/cypress/integration/habit.spec.js
+++ cypress/project-1/cypress/integration/habit.spec.js
@@ -8,4 +8,14 @@ describe("habit dashboard",()=>{
         cy.contains("button","Add").click()
         cy.contains("Add a new habit").should("be.visible")
     })
+
+    it("should display habit card when new habit is added",()=>{
+        cy.get("#habit-add-btn").click()
+        // cy.get(".modal-content").should("be.visible")
+        cy.get("input[class='form-control']").type("drink a big cup of water dude!")
+        cy.contains("Save Changes").click()
+        cy.contains("drink a big cup of water dude!")
+          .should("be.visible")
+          .and("have.class","HabitCard__habit-container")
+    })
 })
