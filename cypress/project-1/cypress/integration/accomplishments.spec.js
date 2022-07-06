f294c4e5492b9cbac045ba4ec766d1c3dab4859f
Muhiddin <muhiddinjv@gmail.com>
2022-07-06 15:07:41 +0500

    test: validation component is visible after submit

--- cypress/project-1/cypress/integration/accomplishments.spec.js
+++ cypress/project-1/cypress/integration/accomplishments.spec.js
@@ -1,12 +1,26 @@
+const { waitFor } = require("@testing-library/react")
+
 describe("Accomplishments Dashboard",()=>{
     beforeEach(()=>{
         cy.visit("/accomplishments")
     })
 
     it("should showcase error if info is missing",()=>{
-        cy.get(".Accomplishment-input")
-        cy.get(".Accomplishment-textarea")
-        cy.get("[data-cy='accomplishment-checkbox']")
+        cy.get("[data-cy='accomplishment-title-input']").type("My basketball accomplishment")
+        cy.get("[data-cy='accomplishment-input']").type("I made the hoop 10x in a row, dude!")
+        cy.contains("Submit Accomplishment").click();
+        cy.contains("Complete the items above to continue").should("be.visible")
+    })
+
+    it("should display validation component if all info is input",()=>{
+        cy.get("[data-cy='accomplishment-title-input']").type("My basketball accomplishment")
+        cy.get("[data-cy='accomplishment-input']").type("I made the hoop 10x in a row, dude!")
+        cy.get("[type='checkbox']").click();
+        cy.contains("Submit Accomplishment").click();
+        cy.get(".Accomplishment-spinner-container").should("be.visible")
+        cy.contains("This Accomplisment was Successfully Submitted").should("be.visible")
+        
+
     })
 })
 
